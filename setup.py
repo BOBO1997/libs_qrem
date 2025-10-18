c5da5aa012344e281342f0506514a964db7ffb30
Bo Yang <yh198595@gmail.com>
2025-10-18 14:29:35 +0200

    updated setup.py and added pyproject.toml

--- setup.py
+++ setup.py
@@ -1,46 +1,33 @@
-import os
-import sys
-import subprocess
-import setuptools
-
-# try:
-#     import numpy as np
-# except ImportError:
-#     subprocess.call([sys.executable, '-m', 'pip', 'install', 'numpy'])
-#     import numpy as np
-    
-# try:
-#     from Cython.Build import cythonize
-# except ImportError:
-#     subprocess.call([sys.executable, '-m', 'pip', 'install', 'cython'])
-#     from Cython.Build import cythonize
-
-from setuptools import setup, Extension, find_packages
-import numpy as np
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
 from Cython.Build import cythonize
-from Cython.Distutils import build_ext
-from distutils.core import setup
+
+class build_ext(_build_ext):
+    def finalize_options(self):
+        super().finalize_options()
+        import numpy  # ← ここで遅延 import（ビルド環境に既に入っている）
+        self.include_dirs.append(numpy.get_include())
 
 ext_modules = [
     Extension(
         "libs_qrem",
         sources=[
-            "./libs_qrem/libs_qrem.pyx",
-            "./cpp/eigen_utils.cpp",
-            "./cpp/combinations.cpp",
-            "./cpp/hamming.cpp",
-            "./cpp/sgs_algorithm.cpp",
-            "./cpp/harger_higham.cpp",
-            "./cpp/qrem_filter.cpp",
-            "./cpp/ignis_filter.cpp",
-            "./cpp/delta_filter.cpp",
-            "./cpp/least_norm_filter.cpp",
-            "./cpp/mooney_etal_filter.cpp",
-            "./cpp/nation_etal_filter.cpp",
+            "libs_qrem/libs_qrem.pyx",
+            "cpp/eigen_utils.cpp",
+            "cpp/combinations.cpp",
+            "cpp/hamming.cpp",
+            "cpp/sgs_algorithm.cpp",
+            "cpp/harger_higham.cpp",
+            "cpp/qrem_filter.cpp",
+            "cpp/ignis_filter.cpp",
+            "cpp/delta_filter.cpp",
+            "cpp/least_norm_filter.cpp",
+            "cpp/mooney_etal_filter.cpp",
+            "cpp/nation_etal_filter.cpp",
         ],
+        language="c++",
         extra_compile_args=["-std=c++17", "-pthread"],
-		include_dirs=["./eigen"],
-        language="c++"
+        include_dirs=["./eigen"],  # numpy の include は build_ext で追加
     ),
 ]
 
@@ -50,10 +37,7 @@ setup(
     description="efficient quantum readout error mitigation library",
     cmdclass={"build_ext": build_ext},
     ext_modules=cythonize(ext_modules, language_level=3),
-    include_dirs=[np.get_include()],
-    zip_safe=False,
     packages=["libs_qrem"],
-    package_dir={
-        "libs_qrem": "libs_qrem"
-    },
+    package_dir={"libs_qrem": "libs_qrem"},
+    zip_safe=False,
 )
